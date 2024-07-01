620ffd409e169eb20409e744d62fa3060ccd85e6
gavino41 <gavinosborne6910@gmail.com>
2024-07-01 16:11:54 +0900

    updated header styles and added a footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import Header from "./header";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,9 +17,12 @@ export default function RootLayout({
         return (
                 <>
                         <html lang="en">
+
                                 <body className={inter.className}>
                                         <Header />
-                                        {children}</body>
+                                        {children}
+                                        <Footer />
+                                </body>
                         </html>
                 </>
         );
---
+++ components/header.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/header.module.css';
+import Link from 'next/link';
+const Header = () => {
+        return (
+                <nav className={styles.header}>
+                        <div className={styles.links}>
+                                <div className={styles.nameDiv}>
+                                        <Link href="/" className={styles.nameText}> Gavin Osborne</Link>
+                                </div>
+                                <div className={styles.linkDiv}>
+                                        <Link href="/" className={styles.linkText}>Home</Link>
+                                        <Link href="/about" className={styles.linkText}>About</Link>
+                                        <Link href="/projects" className={styles.linkText}>Projects</Link>
+                                        <Link href="/contact" className={styles.linkText}>Contact</Link>
+                                </div>
+                        </div>
+                </nav>
+        );
+};
+
+export default Header;
